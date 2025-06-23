3a787dc590344d2e36a69305ae2923c70b877483
CodeNur <dodgechalanger9@gmail.com>
2025-06-23 23:35:47 +0500

    Qo‘shildi: /ascii, /sasuke, /matrix, /skull endpointlar

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const figlet = require("figlet");
+
 const app = express();
 const port = 3000;
 
-const asciiBanner = `
+// Hacked banner
+const hackedBanner = `
  ███╗   ███╗███████╗     ███████╗ █████╗ ███████╗██╗   ██╗██╗  ██╗███████╗
  ████╗ ████║██╔════╝     ██╔════╝██╔══██╗██╔════╝██║   ██║██║ ██╔╝██╔════╝
  ██╔████╔██║█████╗       █████╗  ███████║███████╗██║   ██║█████╔╝ █████╗  
@@ -12,30 +15,37 @@ const asciiBanner = `
              M R .  S A S U K E   H A C K E D !
 `;
 
-app.get("/hacked", (req, res) => {
-  res.set("Content-Type", "text/plain");
-  res.send(asciiBanner);
+app.get("/", (req, res) => {
+  res
+    .type("text/plain")
+    .send(
+      "Salom, bu Mr. Sasuke ASCII serverining bosh sahifasi.\n" +
+        "Quyidagi yo‘llarni sinab ko‘ring:\n" +
+        "/hacked\n/sasuke\n/skull\n/matrix\n/ascii/Sasuke"
+    );
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.get("/hacked", (req, res) => {
+  res.type("text/plain").send(hackedBanner);
 });
-app.get("/", (req, res) => {
-  res.set("Content-Type", "text/plain");
-  res.send(
-    "Salom, bu Mr. Sasuke ASCII serverining bosh sahifasi. /hacked sahifasiga kiring."
-  );
-});
-const figlet = require("figlet");
 
 app.get("/sasuke", (req, res) => {
   figlet.text("SASUKE", { font: "Standard" }, (err, data) => {
-    res.type("text").send(data);
+    if (err) return res.status(500).send("Figlet error");
+    res.type("text/plain").send(data);
   });
 });
 
+app.get("/matrix", (req, res) => {
+  res
+    .type("text/plain")
+    .send(
+      `010101010101010101010101010101\n101010101010101010101010101010\n010101010101010101010101010101\n101010101010101010101010101010`
+    );
+});
+
 app.get("/skull", (req, res) => {
-  res.type("text").send(`
+  res.type("text/plain").send(`
       _____
      /     \\
     | () () |
@@ -45,15 +55,15 @@ app.get("/skull", (req, res) => {
   `);
 });
 
-app.get("/matrix", (req, res) => {
-  res.type("text").send(`
-010101010101010101010101010101
-101010101010101010101010101010
-010101010101010101010101010101
-  `);
-});
 app.get("/ascii/:text", (req, res) => {
-  figlet.text(req.params.text, { font: "Ghost" }, (err, data) => {
-    res.type("text").send(data);
+  const input = req.params.text;
+
+  figlet.text(input, { font: "Standard" }, (err, data) => {
+    if (err) return res.status(500).send("ASCII generation error");
+    res.type("text/plain").send(data);
   });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
