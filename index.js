60a2a2403b0ce63d799d7ee0bd3a561e6667bf8a
CodeNur <dodgechalanger9@gmail.com>
2025-06-23 23:20:45 +0500

    Add root route

--- index.js
+++ index.js
@@ -20,3 +20,9 @@ app.get("/hacked", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+app.get("/", (req, res) => {
+  res.set("Content-Type", "text/plain");
+  res.send(
+    "Salom, bu Mr. Sasuke ASCII serverining bosh sahifasi. /hacked sahifasiga kiring."
+  );
+});
