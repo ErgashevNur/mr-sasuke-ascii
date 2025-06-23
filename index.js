6945b317a4651a06451e64cdf6f3ad1d2457d8b7
CodeNur <dodgechalanger9@gmail.com>
2025-06-23 23:30:44 +0500

    coder

--- index.js
+++ index.js
@@ -26,3 +26,34 @@ app.get("/", (req, res) => {
     "Salom, bu Mr. Sasuke ASCII serverining bosh sahifasi. /hacked sahifasiga kiring."
   );
 });
+const figlet = require("figlet");
+
+app.get("/sasuke", (req, res) => {
+  figlet.text("SASUKE", { font: "Standard" }, (err, data) => {
+    res.type("text").send(data);
+  });
+});
+
+app.get("/skull", (req, res) => {
+  res.type("text").send(`
+      _____
+     /     \\
+    | () () |
+     \\  ^  /
+      |||||
+      |||||
+  `);
+});
+
+app.get("/matrix", (req, res) => {
+  res.type("text").send(`
+010101010101010101010101010101
+101010101010101010101010101010
+010101010101010101010101010101
+  `);
+});
+app.get("/ascii/:text", (req, res) => {
+  figlet.text(req.params.text, { font: "Ghost" }, (err, data) => {
+    res.type("text").send(data);
+  });
+});
